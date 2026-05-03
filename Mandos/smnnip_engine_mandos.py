84018be3c6032785d30ebd2ee10b05c1afe4f235
Cody Allison <the.wandering.god@gmail.com>
2026-05-03 11:10:03 -0700

    Mandos: smnnip_engine_mandos.py

---
+++ Mandos/smnnip_engine_mandos.py
@@ -0,0 +1,11 @@
+"""SMNNIP Instance Engine -- Mandos face."""
+
+FACE_NAME = "Mandos"
+
+def verify(signal):
+    """Verify conservation of checkpoint state. TODO: implement."""
+    raise NotImplementedError("Mandos SMNNIP verify() not yet implemented.")
+
+def sign(signal):
+    """Sign a verified checkpoint. TODO: implement."""
+    raise NotImplementedError("Mandos SMNNIP sign() not yet implemented.")
